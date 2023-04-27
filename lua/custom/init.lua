67289dcc2883a93e82c0b1805e9061bd8025eacc
shiyangzhang <shiyangzhang@whale.im>
2023-04-27 17:39:55 +0800

    save [main]

--- lua/custom/init.lua
+++ lua/custom/init.lua
@@ -1,4 +1,6 @@
 -- vim.api.nvim_command("cd ~/.config/nvim")
 vim.opt.guicursor = "a:blinkon10"
 vim.wo.relativenumber = true
+vim.opt.list = true
+vim.opt.listchars = { space = '⋅', tab = '--' }
 -- vim.opt.colorcolumn = "80"
