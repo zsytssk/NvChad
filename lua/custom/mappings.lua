af2772982529ebf94c059742b5c36d018bd3a532
shiyangzhang <shiyangzhang@whale.im>
2023-05-08 12:14:25 +0800

    [main]

--- lua/custom/mappings.lua
+++ lua/custom/mappings.lua
@@ -5,6 +5,12 @@ local M = {}
 M.general = {
   i = {
     -- save
+    ["<leader>na"] = { "<cmd> Nvdash <CR>", "open nvdash" },
+    ["<C-s>"] = { "<cmd> w  <CR>", "save file" },
+  },
+  n = {
+    -- save
+    ["<leader>na"] = { "<cmd> Nvdash <CR>", "open nvdash" },
     ["<C-s>"] = { "<cmd> w  <CR>", "save file" },
   },
 
